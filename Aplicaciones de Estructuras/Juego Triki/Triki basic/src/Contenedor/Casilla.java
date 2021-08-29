9cc87ebc93c36ac232998f6b5a44de008a72e14d
Felipe Narvaez <luis.narvaez@usantoto.edu.co>
2021-08-29 15:14:34 -0500

    Triki basic

---
+++ Aplicaciones de Estructuras/Juego Triki/Triki basic/src/Contenedor/Casilla.java
@@ -0,0 +1,40 @@
+/*
+ Facultad: Ingenieria de Sistemas
+ Materia: Estructura de Datos
+ Estudiante: Luis Felipe Narvaez Gomez
+ Codigo Estudiante: 2312660
+ Ejercicio: TRIKI 
+ */
+
+package Contenedor;
+
+//import javax.swing.JOptionPane;
+
+public class Casilla
+{
+	private String numero;
+	//private String mostar;
+	
+	public Casilla (String numero)
+	{
+		this.numero = numero;
+	}
+	public Casilla()
+	{
+		
+	}
+
+	
+	
+	
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+	
+}
