bb2d2c25bc95d4ce507142c61ee426243b3bcca4
Felipe Narvaez <luis.narvaez@usantoto.edu.co>
2021-08-29 14:20:46 -0500

    try catch

---
+++ Estructuras Basicas/Calculadora try Catch/src/casio/Numero.java
@@ -0,0 +1,41 @@
+/*
+ Facultad: Ingenieria de Sistemas
+ Estudiante: Luis Felipe Narvaez Gomez
+ Codigo: 2312660
+ Materia: Estructura de datos 
+ Grupo: CONSSF
+ Ejericicio: Calculadora de dos variables con Excepciones
+*/
+
+package casio;
+
+public class Numero 
+{
+	private float number;
+	
+	public Numero()
+	{
+		
+	}
+	
+	public Numero(float number)
+	{
+		this.number = number;
+	}
+
+	
+	public float getNumber() {
+		return number;
+	}
+
+	public void setNumber(float number) {
+		this.number = number;
+	}
+	
+	//public 
+
+	
+	
+	
+
+}
